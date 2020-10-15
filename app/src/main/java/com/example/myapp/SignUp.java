ebdce0ab1da1c1a57ac184e30106f678c744261c
Harsh Dalwe <tcad.tech21@gmail.com>
2020-10-15 21:46:39 +0530

    Added on click

--- app/src/main/java/com/example/myapp/SignUp.java
+++ app/src/main/java/com/example/myapp/SignUp.java
@@ -3,12 +3,26 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        button = (Button)findViewById(R.id.btnSignUp);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Print", "Clicked on button");
+            }
+        });
     }
 }
