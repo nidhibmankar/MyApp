359aac97e372010079a481ecabdf36be1902f296
nidhi mankar <nidhibmankar@gmail.com>
2020-11-26 21:07:30 +0530

    Database validation for username

--- app/src/main/java/com/example/myapp/SignUp.java
+++ app/src/main/java/com/example/myapp/SignUp.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -28,12 +29,11 @@ import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
 
-    EditText emailText, passText, mobileText, usernameText,cpasswordText;
+    private EditText emailText, passText, mobileText, usernameText, cpass;
     Button button;
 
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
-
 
 
     @Override
@@ -41,82 +41,101 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+
+        // getting the current instance of the database from the firebase
         firebaseAuth = FirebaseAuth.getInstance();
 
-    //TODO: new c_pass ; validate email , password , mobile number;
-        usernameText = (EditText)findViewById(R.id.username);   //username
-        emailText = (EditText)findViewById(R.id.email);         //email
-        mobileText = (EditText)findViewById(R.id.mobile);       //mobile
-        passText = (EditText)findViewById(R.id.SignUPpassword); //password
-        cpasswordText = (EditText)findViewById(R.id.SignUPpassword); //confim password
+        //TODO: new c_pass ; validate email , password , mobile number;
 
+        // Initialization
+        usernameText = (EditText) findViewById(R.id.username);   //username
+        emailText = (EditText) findViewById(R.id.email);         //email
+        mobileText = (EditText) findViewById(R.id.mobile);       //mobile
+        passText = (EditText) findViewById(R.id.SignUPpassword); //password
+        cpass = (EditText) findViewById(R.id.SignUPCnfpassword); //confirm password
 
+        button = (Button) findViewById(R.id.btnSignUp);
 
-
-        button = (Button)findViewById(R.id.btnSignUp);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                final String email = emailText.getText().toString();
-                final String pass = passText.getText().toString();
-                final String mobile = mobileText.getText().toString();
-                final String username = usernameText.getText().toString();
+
+              final String email = emailText.getText().toString();
+              final String pass = passText.getText().toString();
+              final String mobile = mobileText.getText().toString();
+              final String username = usernameText.getText().toString();
 
 
-               Query usernameQuery = FirebaseDatabase.getInstance().getReference().child("Users").orderByChild("username").equalTo(username);
-               usernameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                   @Override
-                   public void onDataChange(@NonNull DataSnapshot snapshot) {
-                       if (snapshot.getChildrenCount() > 0)
-                       {
-                           Toast.makeText(SignUp.this, "Username already exist", Toast.LENGTH_LONG).show();
-                       }
-                       else
-                       {
-                           firebaseAuth.createUserWithEmailAndPassword(email,pass)
-                                   .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
-                                       @Override
-                                       public void onComplete(@NonNull Task<AuthResult> task) {
-                                           if (task.isSuccessful()){
-                                               Toast.makeText(SignUp.this, "Sign Up Success", Toast.LENGTH_SHORT).show();
+                Query usermobileQuery = FirebaseDatabase.getInstance().getReference().child("Users").orderByChild("username").equalTo(username);
 
-                                               // Get current Userid
-                                               String Userid = firebaseAuth.getCurrentUser().getUid();
+                usermobileQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.getChildrenCount() > 0) {
+                            Toast.makeText(SignUp.this, "Username already exist", Toast.LENGTH_LONG).show();
+                        }
 
-                                               // Get Database reference of current user against userid
-                                               DatabaseReference Current_User_Referance = FirebaseDatabase.getInstance().getReference().child("Users").child(Userid);
+                        else {
+                            firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                                    .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(SignUp.this, "Sign Up Success", Toast.LENGTH_SHORT).show();
 
-                                               // Create Map to read and write
-                                               Map userPost = new HashMap();
-                                               userPost.put("username",username);
-                                               userPost.put("email",email);
-                                               userPost.put("mobile",mobile);
-                                               userPost.put("password",pass);
+                                                // Get current Userid
+                                                String Userid = firebaseAuth.getCurrentUser().getUid();
 
-                                               // Save data to database
-                                               Current_User_Referance.setValue(userPost);
-                                           }
-                                           else{
-                                               Toast.makeText(SignUp.this, "SignUP Error", Toast.LENGTH_SHORT).show();
-                                           }
-                                       }
-                                   });
-                       }
-                   }
+                                                // Get Database reference of current user against userid
+                                                DatabaseReference Current_User_Referance = FirebaseDatabase.getInstance().getReference().child("Users").child(Userid);
 
-                   @Override
-                   public void onCancelled(@NonNull DatabaseError error) {
+                                                // Create Map to read and write
+                                                Map userPost = new HashMap();
+                                                userPost.put("username", username);
+                                                userPost.put("email", email);
+                                                userPost.put("mobile", mobileText);
+                                                userPost.put("password", pass);
 
-                   }
-               });
+                                                // Save data to database
+                                                Current_User_Referance.setValue(userPost);
+                                            } else {
+                                                Toast.makeText(SignUp.this, "SignUP Error", Toast.LENGTH_SHORT).show();
+                                            }
 
+
+//                                           if(TextUtils.isEmpty(pass)){
+//                                               passText.setError("Password is required");
+//                                               return;
+//                                           }
+//
+//                                           if(pass.length() < 6){
+//                                               passText.setError("Password must be greater than 6");
+//                                               return;
+//                                           }
+
+
+                                        }
+                                    });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(SignUp.this, "SignUp cancel : " + error, Toast.LENGTH_SHORT).show();
+
+                    }
+                });
 
 
             }
+
+
         });
     }
+
+
 
     @Override
     public void onBackPressed() {
